f6881dfccc0911762d041ff0b59db225990f78b2
Dustin Campbell <dustin@teamcampbell.org>
2010-11-24 15:47:55 -0800

    Object view populates with objects from story file when opened

---
+++ Source/ZDebug.UI/ViewModel/ObjectViewModel.cs
@@ -0,0 +1,19 @@
+﻿using ZDebug.Core.Objects;
+
+namespace ZDebug.UI.ViewModel
+{
+    internal sealed class ObjectViewModel : ViewModelBase
+    {
+        private readonly ZObject obj;
+
+        public ObjectViewModel(ZObject obj)
+        {
+            this.obj = obj;
+        }
+
+        public int Number
+        {
+            get { return obj.Number; }
+        }
+    }
+}
--- Source/ZDebug.UI/ViewModel/ObjectsViewModel.cs
+++ Source/ZDebug.UI/ViewModel/ObjectsViewModel.cs
@@ -1,12 +1,59 @@
 ﻿using System.Windows.Controls;
+using ZDebug.UI.Services;
+using ZDebug.UI.Utilities;
 
 namespace ZDebug.UI.ViewModel
 {
     internal sealed class ObjectsViewModel : ViewModelWithViewBase<UserControl>
     {
+        private readonly BulkObservableCollection<ObjectViewModel> objects;
+
         public ObjectsViewModel()
             : base("ObjectsView")
         {
+            objects = new BulkObservableCollection<ObjectViewModel>();
         }
+
+        private void DebuggerService_StoryOpened(object sender, StoryEventArgs e)
+        {
+            objects.BeginBulkOperation();
+            try
+            {
+                foreach (var obj in e.Story.ObjectTable)
+                {
+                    objects.Add(new ObjectViewModel(obj));
+                }
+            }
+            finally
+            {
+                objects.EndBulkOperation();
+            }
+
+            PropertyChanged("HasStory");
+        }
+
+        private void DebuggerService_StoryClosed(object sender, StoryEventArgs e)
+        {
+            objects.Clear();
+
+            PropertyChanged("HasStory");
+        }
+
+        protected internal override void Initialize()
+        {
+            DebuggerService.StoryOpened += DebuggerService_StoryOpened;
+            DebuggerService.StoryClosed += DebuggerService_StoryClosed;
+        }
+
+        public bool HasStory
+        {
+            get { return DebuggerService.HasStory; }
+        }
+
+        public BulkObservableCollection<ObjectViewModel> Objects
+        {
+            get { return objects; }
+        }
+
     }
 }
