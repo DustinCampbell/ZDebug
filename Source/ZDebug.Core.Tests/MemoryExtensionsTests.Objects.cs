27ce7e6d631b75c3fea0f3d801a48a1fb23b1bd3
Dustin Campbell <dustin@teamcampbell.org>
2010-11-24 11:18:00 -0800

    Split MemoryExtensionsTests into partial classes

---
+++ Source/ZDebug.Core.Tests/MemoryExtensionsTests.Objects.cs
@@ -0,0 +1,45 @@
+﻿using NUnit.Framework;
+using ZDebug.Core.Basics;
+using ZDebug.Core.Tests.Utilities;
+
+namespace ZDebug.Core.Tests
+{
+    [TestFixture]
+    public partial class MemoryExtensionsTests
+    {
+        [Test, Category(Categories.Memory)]
+        public void ReadObjectParentIndex()
+        {
+            var memory = LoadCZech();
+            Assert.That(memory.ReadObjectParentIndex(6), Is.EqualTo(5));
+        }
+
+        [Test, Category(Categories.Memory)]
+        public void ReadObjectSiblingIndex()
+        {
+            var memory = LoadCZech();
+            Assert.That(memory.ReadObjectSiblingIndex(6), Is.EqualTo(7));
+        }
+
+        [Test, Category(Categories.Memory)]
+        public void ReadObjectChildIndex()
+        {
+            var memory = LoadCZech();
+            Assert.That(memory.ReadObjectChildIndex(7), Is.EqualTo(8));
+        }
+
+        [Test, Category(Categories.Memory)]
+        public void ReadObjectPropertyTableAddress()
+        {
+            var memory = LoadCZech();
+            Assert.That(memory.ReadObjectPropertyTableAddress(7), Is.EqualTo(0x028e));
+        }
+
+        [Test, Category(Categories.Memory)]
+        public void GetObjectCount()
+        {
+            var memory = LoadCZech();
+            Assert.That(memory.GetObjectCount(), Is.EqualTo(10));
+        }
+    }
+}
