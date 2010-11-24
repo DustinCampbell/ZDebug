015ec90a96cfa559117108399759670eec020c35
Dustin Campbell <dustin@teamcampbell.org>
2010-11-24 14:31:09 -0800

    Added more unit tests for object/property table APIs

--- Source/ZDebug.Core.Tests/StoryTests.Objects.cs
+++ Source/ZDebug.Core.Tests/StoryTests.Objects.cs
@@ -6,39 +6,74 @@ namespace ZDebug.Core.Tests
     [TestFixture]
     public partial class StoryTests
     {
+        [Test, Category(Categories.Story)]
+        public void Curses_ObjectTable_Count()
+        {
+            var story = LoadCurses();
+            Assert.That(story.ObjectTable.Count, Is.EqualTo(502));
+        }
+
         [Test, Category(Categories.Memory)]
-        public void Curses_ObjectTable_478_ParentNumber()
+        public void Curses_ObjectTable_478_Parent_Number()
         {
             var story = LoadCurses();
             Assert.That(story.ObjectTable.GetByNumber(478).Parent.Number, Is.EqualTo(477));
         }
 
         [Test, Category(Categories.Memory)]
-        public void Curses_ObjectTable_478_SiblingNumber()
+        public void Curses_ObjectTable_478_Sibling_Number()
         {
             var story = LoadCurses();
             Assert.That(story.ObjectTable.GetByNumber(478).Sibling.Number, Is.EqualTo(479));
         }
 
         [Test, Category(Categories.Memory)]
-        public void Curses_ObjectTable_478_ChildNumber()
+        public void Curses_ObjectTable_478_Child_Number()
         {
             var story = LoadCurses();
             Assert.That(story.ObjectTable.GetByNumber(478).Child.Number, Is.EqualTo(483));
         }
 
         [Test, Category(Categories.Memory)]
-        public void Curses_ObjectTable_478_PropertyTableAddress()
+        public void Curses_ObjectTable_478_PropertyTable_Address()
         {
             var story = LoadCurses();
             Assert.That(story.ObjectTable.GetByNumber(478).PropertyTable.Address, Is.EqualTo(0x5802));
         }
 
         [Test, Category(Categories.Memory)]
-        public void Curses_ObjectTable_478_PropertyTableCount()
+        public void Curses_ObjectTable_478_PropertyTable_Count()
         {
             var story = LoadCurses();
             Assert.That(story.ObjectTable.GetByNumber(478).PropertyTable.Count, Is.EqualTo(4));
+        }
+
+        [Test, Category(Categories.Memory)]
+        public void Curses_ObjectTable_478_PropertyTable_18_DataLength()
+        {
+            var story = LoadCurses();
+            Assert.That(story.ObjectTable.GetByNumber(478).PropertyTable.GetByNumber(18).DataLength, Is.EqualTo(2));
+        }
+
+        [Test, Category(Categories.Memory)]
+        public void Curses_ObjectTable_478_PropertyTable_17_DataLength()
+        {
+            var story = LoadCurses();
+            Assert.That(story.ObjectTable.GetByNumber(478).PropertyTable.GetByNumber(17).DataLength, Is.EqualTo(2));
+        }
+
+        [Test, Category(Categories.Memory)]
+        public void Curses_ObjectTable_478_PropertyTable_4_DataLength()
+        {
+            var story = LoadCurses();
+            Assert.That(story.ObjectTable.GetByNumber(478).PropertyTable.GetByNumber(4).DataLength, Is.EqualTo(2));
+        }
+
+        [Test, Category(Categories.Memory)]
+        public void Curses_ObjectTable_478_PropertyTable_1_DataLength()
+        {
+            var story = LoadCurses();
+            Assert.That(story.ObjectTable.GetByNumber(478).PropertyTable.GetByNumber(1).DataLength, Is.EqualTo(8));
         }
 
         [Test, Category(Categories.Memory)]
@@ -77,13 +112,6 @@ namespace ZDebug.Core.Tests
         }
 
         [Test, Category(Categories.Story)]
-        public void Curses_ObjectTable_Count()
-        {
-            var story = LoadCurses();
-            Assert.That(story.ObjectTable.Count, Is.EqualTo(502));
-        }
-
-        [Test, Category(Categories.Story)]
         public void CZech_ObjectTable_Count()
         {
             var story = LoadCZech();
@@ -95,6 +123,76 @@ namespace ZDebug.Core.Tests
         {
             var story = LoadZork1();
             Assert.That(story.ObjectTable.Count, Is.EqualTo(250));
+        }
+
+        [Test, Category(Categories.Memory)]
+        public void Zork1_ObjectTable_172_Parent_Number()
+        {
+            var story = LoadZork1();
+            Assert.That(story.ObjectTable.GetByNumber(172).Parent.Number, Is.EqualTo(82));
+        }
+
+        [Test, Category(Categories.Memory)]
+        public void Zork1_ObjectTable_172_Sibling_Number()
+        {
+            var story = LoadZork1();
+            Assert.That(story.ObjectTable.GetByNumber(172).Sibling.Number, Is.EqualTo(100));
+        }
+
+        [Test, Category(Categories.Memory)]
+        public void Zork1_ObjectTable_172_Child_Number()
+        {
+            var story = LoadZork1();
+            Assert.That(story.ObjectTable.GetByNumber(172).Child.Number, Is.EqualTo(173));
+        }
+
+        [Test, Category(Categories.Memory)]
+        public void Zork1_ObjectTable_172_PropertyTable_Address()
+        {
+            var story = LoadZork1();
+            Assert.That(story.ObjectTable.GetByNumber(172).PropertyTable.Address, Is.EqualTo(0x1b75));
+        }
+
+        [Test, Category(Categories.Memory)]
+        public void Zork1_ObjectTable_172_PropertyTable_Count()
+        {
+            var story = LoadZork1();
+            Assert.That(story.ObjectTable.GetByNumber(172).PropertyTable.Count, Is.EqualTo(5));
+        }
+
+        [Test, Category(Categories.Memory)]
+        public void Zork1_ObjectTable_172_PropertyTable_31_DataLength()
+        {
+            var story = LoadZork1();
+            Assert.That(story.ObjectTable.GetByNumber(172).PropertyTable.GetByNumber(31).DataLength, Is.EqualTo(1));
+        }
+
+        [Test, Category(Categories.Memory)]
+        public void Zork1_ObjectTable_172_PropertyTable_28_DataLength()
+        {
+            var story = LoadZork1();
+            Assert.That(story.ObjectTable.GetByNumber(172).PropertyTable.GetByNumber(28).DataLength, Is.EqualTo(4));
+        }
+
+        [Test, Category(Categories.Memory)]
+        public void Zork1_ObjectTable_172_PropertyTable_17_DataLength()
+        {
+            var story = LoadZork1();
+            Assert.That(story.ObjectTable.GetByNumber(172).PropertyTable.GetByNumber(17).DataLength, Is.EqualTo(2));
+        }
+
+        [Test, Category(Categories.Memory)]
+        public void Zork1_ObjectTable_172_PropertyTable_5_DataLength()
+        {
+            var story = LoadZork1();
+            Assert.That(story.ObjectTable.GetByNumber(172).PropertyTable.GetByNumber(5).DataLength, Is.EqualTo(2));
+        }
+
+        [Test, Category(Categories.Memory)]
+        public void Zork1_ObjectTable_172_PropertyTable_4_DataLength()
+        {
+            var story = LoadZork1();
+            Assert.That(story.ObjectTable.GetByNumber(172).PropertyTable.GetByNumber(4).DataLength, Is.EqualTo(4));
         }
     }
 }
