8ee2b3b890d2a4990289d3cea7d035af6fe241ca
Dustin Campbell <dustin@teamcampbell.org>
2010-11-25 03:13:49 -0800

    Added missing file

---
+++ Source/ZDebug.UI/ViewModel/PropertyViewModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using ZDebug.Core.Objects;
+
+namespace ZDebug.UI.ViewModel
+{
+    internal sealed class PropertyViewModel : ViewModelBase
+    {
+        private readonly ZProperty property;
+
+        public PropertyViewModel(ZProperty property)
+        {
+            this.property = property;
+        }
+
+        public int Number
+        {
+            get { return property.Number; }
+        }
+
+        public string DataDisplayText
+        {
+            get
+            {
+                var bytes = property.ReadAsBytes();
+                var byteStrings = Array.ConvertAll(bytes, b => b.ToString("x2"));
+                return string.Join(" ", byteStrings);
+            }
+        }
+    }
+}
