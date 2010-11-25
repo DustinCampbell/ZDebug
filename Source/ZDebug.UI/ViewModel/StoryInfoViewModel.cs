2ec6a81de70f4c5f94d7cc8053a48ef6788e8d6d
Dustin Campbell <dustin@teamcampbell.org>
2010-11-25 13:41:35 -0800

    Added story info view and tool window on left side

---
+++ Source/ZDebug.UI/ViewModel/StoryInfoViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Controls;
+
+namespace ZDebug.UI.ViewModel
+{
+    internal sealed class StoryInfoViewModel : ViewModelWithViewBase<UserControl>
+    {
+        public StoryInfoViewModel()
+            : base("StoryInfoView")
+        {
+        }
+    }
+}
