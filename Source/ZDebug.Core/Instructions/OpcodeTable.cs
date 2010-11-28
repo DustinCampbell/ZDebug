dad82d97209ca49173e5e02c3aebfd80276135ba
Dustin Campbell <dustin@teamcampbell.org>
2010-11-28 15:30:40 -0800

    Added all two-operand opcodes

--- Source/ZDebug.Core/Instructions/OpcodeTable.cs
+++ Source/ZDebug.Core/Instructions/OpcodeTable.cs
@@ -11,6 +11,37 @@ namespace ZDebug.Core.Instructions
         {
             opcodeMap = new Dictionary<Tuple<OpcodeKind, byte, byte>, Opcode>();
 
+            // two-operand opcodes
+            AddOpcode(OpcodeKind.TwoOp, 0x01, "je", OpcodeFlags.Branch);
+            AddOpcode(OpcodeKind.TwoOp, 0x02, "jl", OpcodeFlags.Branch);
+            AddOpcode(OpcodeKind.TwoOp, 0x03, "jg", OpcodeFlags.Branch);
+            AddOpcode(OpcodeKind.TwoOp, 0x04, "dec_chk", OpcodeFlags.Branch | OpcodeFlags.FirstOpByRef);
+            AddOpcode(OpcodeKind.TwoOp, 0x05, "inc_chk", OpcodeFlags.Branch | OpcodeFlags.FirstOpByRef);
+            AddOpcode(OpcodeKind.TwoOp, 0x06, "jin", OpcodeFlags.Branch);
+            AddOpcode(OpcodeKind.TwoOp, 0x07, "test", OpcodeFlags.Branch);
+            AddOpcode(OpcodeKind.TwoOp, 0x08, "or", OpcodeFlags.Store);
+            AddOpcode(OpcodeKind.TwoOp, 0x09, "and", OpcodeFlags.Store);
+            AddOpcode(OpcodeKind.TwoOp, 0x0a, "test_attr", OpcodeFlags.Branch);
+            AddOpcode(OpcodeKind.TwoOp, 0x0b, "set_attr");
+            AddOpcode(OpcodeKind.TwoOp, 0x0c, "clear_attr");
+            AddOpcode(OpcodeKind.TwoOp, 0x0d, "store", OpcodeFlags.FirstOpByRef);
+            AddOpcode(OpcodeKind.TwoOp, 0x0e, "insert_obj");
+            AddOpcode(OpcodeKind.TwoOp, 0x0f, "loadw", OpcodeFlags.Store);
+            AddOpcode(OpcodeKind.TwoOp, 0x10, "loadb", OpcodeFlags.Store);
+            AddOpcode(OpcodeKind.TwoOp, 0x11, "get_prop", OpcodeFlags.Store);
+            AddOpcode(OpcodeKind.TwoOp, 0x12, "get_prop_addr", OpcodeFlags.Store);
+            AddOpcode(OpcodeKind.TwoOp, 0x13, "get_next_prop", OpcodeFlags.Store);
+            AddOpcode(OpcodeKind.TwoOp, 0x14, "add", OpcodeFlags.Store);
+            AddOpcode(OpcodeKind.TwoOp, 0x15, "sub", OpcodeFlags.Store);
+            AddOpcode(OpcodeKind.TwoOp, 0x16, "mul", OpcodeFlags.Store);
+            AddOpcode(OpcodeKind.TwoOp, 0x17, "div", OpcodeFlags.Store);
+            AddOpcode(OpcodeKind.TwoOp, 0x18, "mod", OpcodeFlags.Store);
+            AddOpcode(OpcodeKind.TwoOp, 0x19, "call_2s", OpcodeFlags.Call | OpcodeFlags.Store, fromVersion: 4);
+            AddOpcode(OpcodeKind.TwoOp, 0x1a, "call_2n", OpcodeFlags.Call, fromVersion: 5);
+            AddOpcode(OpcodeKind.TwoOp, 0x1b, "set_color", fromVersion: 5, toVersion: 5);
+            AddOpcode(OpcodeKind.TwoOp, 0x1b, "set_color", fromVersion: 6);
+            AddOpcode(OpcodeKind.TwoOp, 0x1c, "throw", fromVersion: 5);
+
             // one-operand opcodes
             AddOpcode(OpcodeKind.OneOp, 0x0f, "call_1n", OpcodeFlags.Call, fromVersion: 5);
 
