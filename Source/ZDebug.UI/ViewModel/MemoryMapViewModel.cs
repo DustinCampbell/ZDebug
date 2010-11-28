47f5a44b702a119a48f22c1ff2733cd64614b874
Dustin Campbell <dustin@teamcampbell.org>
2010-11-27 17:40:00 -0800

    Added view to display memory map

--- Source/ZDebug.UI/ViewModel/MemoryMapViewModel.cs
+++ Source/ZDebug.UI/ViewModel/MemoryMapViewModel.cs
@@ -1,16 +1,49 @@
-﻿using System.Windows.Controls;
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Windows.Controls;
+using ZDebug.UI.Services;
 
 namespace ZDebug.UI.ViewModel
 {
     internal class MemoryMapViewModel : ViewModelWithViewBase<UserControl>
     {
+        private ReadOnlyCollection<MemoryMapRegionViewModel> regions;
+
         public MemoryMapViewModel()
             : base("MemoryMapView")
         {
         }
 
+        private void DebuggerService_StoryOpened(object sender, StoryEventArgs e)
+        {
+            var list = new List<MemoryMapRegionViewModel>();
+
+            foreach (var region in e.Story.MemoryMap)
+            {
+                list.Add(new MemoryMapRegionViewModel(region));
+            }
+
+            regions = new ReadOnlyCollection<MemoryMapRegionViewModel>(list);
+
+            PropertyChanged("Regions");
+        }
+
+        private void DebuggerService_StoryClosed(object sender, StoryEventArgs e)
+        {
+            regions = null;
+
+            PropertyChanged("Regions");
+        }
+
         protected internal override void Initialize()
         {
+            DebuggerService.StoryOpened += DebuggerService_StoryOpened;
+            DebuggerService.StoryClosed += DebuggerService_StoryClosed;
+        }
+
+        public ReadOnlyCollection<MemoryMapRegionViewModel> Regions
+        {
+            get { return regions; }
         }
     }
 }
