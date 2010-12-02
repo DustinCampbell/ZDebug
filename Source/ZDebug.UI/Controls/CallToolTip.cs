3e3a4b279eb96c3c589fad718fd63652abeeb96f
Dustin Campbell <dustin@teamcampbell.org>
2010-12-02 15:00:51 -0800

    Added missing files

---
+++ Source/ZDebug.UI/Controls/CallToolTip.cs
@@ -0,0 +1,13 @@
+﻿using System.Windows.Controls;
+using ZDebug.Core.Execution;
+
+namespace ZDebug.UI.Controls
+{
+    public class CallToolTip : TextBlock
+    {
+        public CallToolTip(StackFrame frame)
+        {
+            this.Text = "Execution will return here when " + frame.Address.ToString("x4") + " returns.";
+        }
+    }
+}
---
+++ Source/ZDebug.UI/ViewModel/StackFrameViewModel.cs
@@ -0,0 +1,16 @@
+﻿using ZDebug.Core.Execution;
+
+namespace ZDebug.UI.ViewModel
+{
+    internal sealed class StackFrameViewModel : ViewModelBase
+    {
+        private readonly StackFrame currentFrame;
+        private readonly StackFrame priorFrame;
+
+        public StackFrameViewModel(StackFrame currentFrame, StackFrame priorFrame)
+        {
+            this.currentFrame = currentFrame;
+            this.priorFrame = priorFrame;
+        }
+    }
+}
