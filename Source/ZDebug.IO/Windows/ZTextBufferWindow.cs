fa3567ae10fd85e477d6df2f4a1aa2b11879f617
Dustin Campbell <dustin@teamcampbell.org>
2010-12-23 12:34:13 -0800

    Changed input text to be inline in the lower window rather than overlay it. This addresses issue #2

--- Source/ZDebug.IO/Windows/ZTextBufferWindow.cs
+++ Source/ZDebug.IO/Windows/ZTextBufferWindow.cs
@@ -6,6 +6,7 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using ZDebug.IO.Services;
+using System.Windows.Threading;
 
 namespace ZDebug.IO.Windows
 {
@@ -16,11 +17,6 @@ namespace ZDebug.IO.Windows
         private readonly Paragraph paragraph;
         private readonly Size fontCharSize;
 
-        private readonly Grid inputGrid;
-        private readonly TextBox inputTextBox;
-        private readonly RowDefinition inputUpperMarginRow;
-        private readonly ColumnDefinition inputLeftMarginColumn;
-
         private bool bold;
         private bool italic;
         private bool fixedPitch;
@@ -29,44 +25,23 @@ namespace ZDebug.IO.Windows
         internal ZTextBufferWindow(ZWindowManager manager)
             : base(manager)
         {
-            this.document = new FlowDocument();
-            this.document.FontFamily = FontsAndColorsService.NormalFontFamily;
-            this.document.FontSize = FontsAndColorsService.FontSize;
-            this.document.PagePadding = new Thickness(8);
-            this.paragraph = new Paragraph();
-            this.document.Blocks.Add(paragraph);
+            document = new FlowDocument
+            {
+                FontFamily = FontsAndColorsService.NormalFontFamily,
+                FontSize = FontsAndColorsService.FontSize,
+                PagePadding = new Thickness(8)
+            };
 
-            this.scrollViewer = new FlowDocumentScrollViewer();
-            this.scrollViewer.FocusVisualStyle = null;
-            this.scrollViewer.Document = this.document;
+            paragraph = new Paragraph();
+            document.Blocks.Add(paragraph);
 
-            this.Children.Add(scrollViewer);
+            scrollViewer = new FlowDocumentScrollViewer
+            {
+                FocusVisualStyle = null,
+                Document = document
+            };
 
-            this.inputGrid = new Grid();
-            this.inputGrid.Visibility = Visibility.Collapsed;
-
-            this.inputUpperMarginRow = new RowDefinition();
-            var inputRow = new RowDefinition();
-            this.inputGrid.RowDefinitions.Add(inputUpperMarginRow);
-            this.inputGrid.RowDefinitions.Add(inputRow);
-
-            this.inputLeftMarginColumn = new ColumnDefinition();
-            var inputColumn = new ColumnDefinition();
-            this.inputGrid.ColumnDefinitions.Add(inputLeftMarginColumn);
-            this.inputGrid.ColumnDefinitions.Add(inputColumn);
-
-            this.inputTextBox = new TextBox();
-            this.inputTextBox.FontFamily = FontsAndColorsService.NormalFontFamily;
-            this.inputTextBox.FontSize = FontsAndColorsService.FontSize;
-            this.inputTextBox.Padding = new Thickness(0);
-            this.inputTextBox.BorderBrush = Brushes.Transparent;
-            this.inputTextBox.BorderThickness = new Thickness(0);
-            Grid.SetRow(inputTextBox, 1);
-            Grid.SetColumn(inputTextBox, 1);
-
-            inputGrid.Children.Add(inputTextBox);
-
-            this.Children.Add(inputGrid);
+            Children.Add(scrollViewer);
 
             var zero = new FormattedText(
                 textToFormat: "0",
@@ -137,18 +112,18 @@ namespace ZDebug.IO.Windows
 
         public override void Clear()
         {
-            this.paragraph.Inlines.Clear();
+            paragraph.Inlines.Clear();
         }
 
         public override void PutString(string text)
         {
-            this.paragraph.Inlines.Add(GetFormattedRun(text));
+            paragraph.Inlines.Add(GetFormattedRun(text));
             FindScrollViewer().ScrollToEnd();
         }
 
         public override void PutChar(char ch)
         {
-            this.paragraph.Inlines.Add(GetFormattedRun(ch.ToString()));
+            paragraph.Inlines.Add(GetFormattedRun(ch.ToString()));
             FindScrollViewer().ScrollToEnd();
         }
 
@@ -157,16 +132,33 @@ namespace ZDebug.IO.Windows
             TextCompositionEventHandler handler = null;
             handler = (s, e) =>
             {
-                this.scrollViewer.TextInput -= handler;
+                scrollViewer.TextInput -= handler;
                 callback(e.Text[0]);
             };
 
-            this.scrollViewer.TextInput += handler;
-            Keyboard.Focus(this.scrollViewer);
+            scrollViewer.TextInput += handler;
+            Keyboard.Focus(scrollViewer);
         }
 
         public override void ReadCommand(int maxChars, Action<string> callback)
         {
+            var inputTextBox = new TextBox()
+            {
+                FontFamily = FontsAndColorsService.NormalFontFamily,
+                FontSize = FontsAndColorsService.FontSize,
+                Padding = new Thickness(0),
+                Margin = new Thickness(0),
+                BorderBrush = Brushes.Transparent,
+                BorderThickness = new Thickness(0)
+            };
+
+            var inlineUIContainer = new InlineUIContainer(inputTextBox, document.ContentEnd)
+            {
+                BaselineAlignment = BaselineAlignment.TextBottom
+            };
+
+            paragraph.Inlines.Add(inlineUIContainer);
+
             KeyEventHandler handler = null;
             handler = (s, e) =>
             {
@@ -175,27 +167,22 @@ namespace ZDebug.IO.Windows
                     return;
                 }
 
-                this.inputTextBox.KeyUp -= handler;
+                inputTextBox.KeyUp -= handler;
 
-                var text = this.inputTextBox.Text;
+                var text = inputTextBox.Text;
+                paragraph.Inlines.Remove(inlineUIContainer);
+
                 PutString(text + '\n');
-
-                this.inputTextBox.Clear();
-                this.inputGrid.Visibility = Visibility.Collapsed;
 
                 callback(text);
             };
 
-            var pointer = this.document.ContentEnd;
-            var rect = pointer.GetCharacterRect(LogicalDirection.Backward);
-
-            this.inputLeftMarginColumn.Width = new GridLength(rect.Right, GridUnitType.Pixel);
-            this.inputUpperMarginRow.Height = new GridLength(rect.Top, GridUnitType.Pixel);
-            this.inputGrid.Visibility = Visibility.Visible;
-
-            this.inputTextBox.MaxLength = maxChars;
-            this.inputTextBox.KeyUp += handler;
-            this.inputTextBox.Focus();
+            inputTextBox.MaxLength = maxChars;
+            inputTextBox.KeyUp += handler;
+            if (!inputTextBox.Focus())
+            {
+                inputTextBox.Dispatcher.BeginInvoke(DispatcherPriority.Input, new Action(delegate { inputTextBox.Focus(); }));
+            }
         }
 
         public override bool SetBold(bool value)
