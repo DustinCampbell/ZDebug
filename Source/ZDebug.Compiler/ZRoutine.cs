d1243987c30d8d418c55dc6c12c80b4f15ff3fa4
Dustin Campbell <dustin@teamcampbell.org>
2010-12-26 13:37:45 -0800

    Added ZDebug.Compiler project

---
+++ Source/ZDebug.Compiler/ZRoutine.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using ZDebug.Core;
+using ZDebug.Core.Instructions;
+using System.Collections.ObjectModel;
+
+namespace ZDebug.Compiler
+{
+    public sealed class ZRoutine
+    {
+        private readonly int address;
+        private readonly ReadOnlyCollection<Instruction> instructions;
+        private readonly Action code;
+
+        public ZRoutine(int address, Story story)
+        {
+            this.address = address;
+        }
+    }
+}
