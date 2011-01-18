b6dc77a7446b2d5141f02e99e2b5594ab7a306fa
Dustin Campbell <dustin@teamcampbell.org>
2011-01-18 06:42:20 -0800

    Using ZDebug.IO library in terp

--- Source/ZDebug.Terp/MainWindow.xaml.cs
+++ Source/ZDebug.Terp/MainWindow.xaml.cs
@@ -14,20 +14,31 @@ using System.Windows.Shapes;
 using Microsoft.Win32;
 using System.IO;
 using ZDebug.Compiler;
+using ZDebug.IO.Windows;
+using ZDebug.Core.Execution;
+using System.Windows.Threading;
 
 namespace ZDebug.Terp
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow : Window, IScreen
     {
+        private ZWindowManager windowManager;
+        private ZWindow mainWindow;
+        private ZWindow upperWindow;
+
+        private ZMachine machine;
+
         public MainWindow()
         {
             InitializeComponent();
+
+            windowManager = new ZWindowManager();
         }
 
-        private void MenuItem_Click(object sender, RoutedEventArgs e)
+        private void Open_Click(object sender, RoutedEventArgs e)
         {
             var dialog = new OpenFileDialog
             {
@@ -41,9 +52,152 @@ namespace ZDebug.Terp
             if (dialog.ShowDialog(this) == true)
             {
                 var memory = File.ReadAllBytes(dialog.FileName);
-                var machine = new ZMachine(memory, new DummyScreen());
+                machine = new ZMachine(memory, this);
+
+                mainWindow = windowManager.Open(ZWindowType.TextBuffer);
+                windowContainer.Children.Add(mainWindow);
+                upperWindow = windowManager.Open(ZWindowType.TextGrid, mainWindow, ZWindowPosition.Above, ZWindowSizeType.Fixed, 0);
+
+                windowManager.Activate(mainWindow);
+
+                Dispatcher.BeginInvoke(new Action(Run), DispatcherPriority.Background);
+            }
+        }
+
+        private void Run()
+        {
+            try
+            {
                 machine.Run();
             }
+            catch
+            {
+            }
+        }
+
+        public void Clear(int window)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ClearAll(bool unsplit = false)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Split(int height)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Unsplit()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SetWindow(int window)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int GetCursorLine()
+        {
+            throw new NotImplementedException();
+        }
+
+        public int GetCursorColumn()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SetCursor(int line, int column)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SetTextStyle(ZTextStyle style)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SetForegroundColor(ZColor color)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SetBackgroundColor(ZColor color)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ZFont SetFont(ZFont font)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ShowStatus()
+        {
+            throw new NotImplementedException();
+        }
+
+        public byte ScreenHeightInLines
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public byte ScreenWidthInColumns
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public ushort ScreenHeightInUnits
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public ushort ScreenWidthInUnits
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public byte FontHeightInUnits
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public byte FontWidthInUnits
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public ZColor DefaultBackgroundColor
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public ZColor DefaultForegroundColor
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public void Print(string text)
+        {
+            windowManager.ActiveWindow.PutString(text);
+        }
+
+        public void Print(char ch)
+        {
+            windowManager.ActiveWindow.PutChar(ch);
+        }
+
+        public void ReadChar(Action<char> callback)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ReadCommand(int maxChars, Action<string> callback)
+        {
+            throw new NotImplementedException();
         }
     }
 }
