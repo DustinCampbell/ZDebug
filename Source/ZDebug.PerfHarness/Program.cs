d64c7f964b066a02efad24060989d36079941097
Dustin Campbell <dustin@teamcampbell.org>
2011-04-25 22:45:59 -0700

    Added bronze script that stops before infinite loop

--- Source/ZDebug.PerfHarness/Program.cs
+++ Source/ZDebug.PerfHarness/Program.cs
@@ -9,7 +9,7 @@
         const string ZORK1 = @"..\..\ZCode\zork1\zork1.z3";
         const string HITCHHIK = @"..\..\ZCode\hitchhik\hitchhik.z5";
 
-        const string BRONZE_SCRIPT = @"..\..\ZCode\bronze\bronze_script.txt";
+        const string BRONZE_SCRIPT = @"..\..\ZCode\bronze\bronze_script_lite.txt";
         const string ROTA_SCRIPT = @"..\..\ZCode\rota\rota_script.txt";
         const string ZORK1_SCRIPT = @"..\..\ZCode\zork1\zork1_script.txt";
         const string HITCHHIK_SCRIPT = @"..\..\ZCode\hitchhik\hitchhik_script.txt";
